ad16296a68079e50cfa7af09a11c207dc39f5300
LalaineHines <lalainehines@gmail.com>
2025-09-26 19:10:24 -0700

    changed script to javascript functionality

--- src/component/utils/DraftDOMTypes.js
+++ src/component/utils/DraftDOMTypes.js
@@ -12,22 +12,22 @@
 'use strict';
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Selection
-export type SelectionObject = {
+export const SelectionObject = {
   /**
    * Returns the Node in which the selection begins. Can return null if
    * selection never existed in the document (e.g., an iframe that was
    * never clicked on). */
-  anchorNode: ?Node,
+  anchorNode: Node,
   anchorOffset: number,
-  focusNode: ?Node,
+  focusNode: Node,
   focusOffset: number,
   isCollapsed: boolean,
   rangeCount: number,
   type: string,
 
-  removeAllRanges(): void,
-  getRangeAt: (index: number) => Range,
-  extend?: (node: Node, offset?: number) => void,
-  addRange: (range: Range) => void,
+  removeAllRanges() {},
+  getRangeAt (index) {},
+  extend: (node, offset),
+  addRange: (range),
   // ...etc. This is a non-exhaustive definition.
 };
