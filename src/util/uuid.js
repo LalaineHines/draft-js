04c0058b482afacc63536b621d25a1a4b9f6cc73
LalaineHines <lalainehines@gmail.com>
2025-09-26 19:04:06 -0700

    changed script to javascript functionality

--- src/util/uuid.js
+++ src/util/uuid.js
@@ -12,7 +12,7 @@
  * Based on the rfc4122-compliant solution posted at
  * http://stackoverflow.com/questions/105034
  */
-function uuid(): string {
+function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = (Math.random() * 16) | 0;
     const v = c == 'x' ? r : (r & 0x3) | 0x8;
