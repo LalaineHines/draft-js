2c29745ddc9e9e8cb3bbe7da81163bce4dfbd57b
LalaineHines <lalainehines@gmail.com>
2025-09-27 19:04:29 -0700

    changed script to javascript functionality

--- src/model/entity/__tests__/getEntityKeyForSelection-test.js
+++ src/model/entity/__tests__/getEntityKeyForSelection-test.js
@@ -36,10 +36,7 @@ const NON_COLLAPSED_SELECTION = initialSelectionState.merge({
 });
 
 const setEntityMutability = (
-  mutability:
-    | $TEMPORARY$string<'IMMUTABLE'>
-    | $TEMPORARY$string<'MUTABLE'>
-    | $TEMPORARY$string<'SEGMENTED'>,
+  mutability
 ) => {
   // $FlowFixMe error encountered when strong-typing ContentState.getEntityMap
   contentState.getEntityMap().__get = () => ({
