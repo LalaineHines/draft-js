9a401d7dbc03f0fc684089e380fa397dc857c70b
LalaineHines <lalainehines@gmail.com>
2025-09-27 20:22:22 -0700

    changed script to javascript gunctionality

--- src/model/immutable/EditorChangeType.js
+++ src/model/immutable/EditorChangeType.js
@@ -11,19 +11,19 @@
 
 'use strict';
 
-export type EditorChangeType =
-  | 'adjust-depth'
-  | 'apply-entity'
-  | 'backspace-character'
-  | 'change-block-data'
-  | 'change-block-type'
-  | 'change-inline-style'
-  | 'move-block'
-  | 'delete-character'
-  | 'insert-characters'
-  | 'insert-fragment'
-  | 'redo'
-  | 'remove-range'
-  | 'spellcheck-change'
-  | 'split-block'
-  | 'undo';
+export const EditorChangeType =
+  'adjust-depth'
+  'apply-entity'
+  'backspace-character'
+  'change-block-data'
+  'change-block-type'
+  'change-inline-style'
+  'move-block'
+  'delete-character'
+  'insert-characters'
+  'insert-fragment'
+  'redo'
+  'remove-range'
+  'spellcheck-change'
+  'split-block'
+  'undo';
